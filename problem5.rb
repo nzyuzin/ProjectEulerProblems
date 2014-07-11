6e21bc67b3b1afaf4d1ec7ab12b2df7284eb5388
Nikita Zyuzin <nzyuzin93@gmail.com>
2014-07-11 16:19:43 +0400

    Add problem5 solution

---
+++ problem5.rb
@@ -0,0 +1,46 @@
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+$TEST_ANSWER
+
+def answer(number)
+  require 'set'
+  factors = Hash.new
+  2.upto(number) do |i|
+    factors_of(i).each do |pair|
+      number = pair[0]
+      power = pair[1]
+      if factors.has_key? number
+        factors[number] = power if factors[number] < power
+      else
+        factors[number] = power
+      end
+    end
+  end
+
+  result = 1
+  factors.each do |e, v|
+    result *= e ** v
+  end
+  result
+end
+
+def factors_of(number)
+  require 'prime'
+  div_result = Array.new
+  Prime.prime_division(number)
+end
+
+def answer_correct?()
+  $TEST_ANSWER = answer(10)
+  $TEST_ANSWER == 2520 
+end
+
+if answer_correct?
+  then puts "#{answer(20)}"
+else
+  # Debug only branch
+  puts "Incorrect answer {#$TEST_ANSWER}!"
+end
+
